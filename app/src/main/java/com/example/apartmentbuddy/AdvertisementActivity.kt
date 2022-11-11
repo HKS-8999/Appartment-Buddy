64e6b389dcb52e94cd90de5bf0b5264982840fa9
Minal Khona <mn977442@dal.ca>
2022-11-11 02:09:05 -0400

    Fragment design for advertisement main page - View Pager

---
+++ app/src/main/java/com/example/apartmentbuddy/AdvertisementActivity.kt
@@ -0,0 +1,11 @@
+package com.example.apartmentbuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdvertisementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advertisement)
+    }
+}
