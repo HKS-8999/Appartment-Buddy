26e88ef536c3c8fdd80ea236c5f054b8f98118e4
Saifali <sf527330@dal.ca>
2022-11-12 14:42:45 -0400

    Corrected the spelling of apartment in the project

--- app/src/androidTest/java/com/example/apartmentbuddy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/apartmentbuddy/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.appartmentbuddy", appContext.packageName)
+        assertEquals("com.example.apartmentbuddy", appContext.packageName)
     }
 }
