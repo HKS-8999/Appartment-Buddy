3b5e0c3d00e837485386247c0ad7acec3efcf14e
Minal Khona <mn977442@dal.ca>
2022-11-14 23:11:03 -0400

    Navigation for create post

--- app/src/main/java/com/example/apartmentbuddy/AdvertisementActivity.kt
+++ app/src/main/java/com/example/apartmentbuddy/AdvertisementActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.apartmentbuddy.databinding.ActivityAdvertisementBinding
 import com.example.apartmentbuddy.fragments.AdvertisementHomeFragment
-import com.example.apartmentbuddy.fragments.PostApartmentFragment
+import com.example.apartmentbuddy.fragments.PostAdvertisementOptions
 
 class AdvertisementActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdvertisementBinding
@@ -22,7 +22,11 @@ class AdvertisementActivity : AppCompatActivity() {
         binding.bottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.adv_home -> replaceFragment(AdvertisementHomeFragment())
-                R.id.adv_newPost -> replaceFragment(PostApartmentFragment())
+                R.id.adv_newPost -> {
+                    supportFragmentManager.beginTransaction()
+                        .add(binding.fragmentContainer.id, PostAdvertisementOptions())
+                        .commit()
+                }
             }
             true
         }
