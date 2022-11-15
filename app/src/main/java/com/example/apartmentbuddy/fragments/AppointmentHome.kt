32d023355e4f907566149ae47720842c66d228a1
Harsh Shah <hr838264@dal.ca>
2022-11-15 08:30:41 -0400

    Removed a file from values and solved the error

---
+++ app/src/main/java/com/example/apartmentbuddy/fragments/AppointmentHome.kt
@@ -0,0 +1,31 @@
+package com.example.apartmentbuddy.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import com.example.apartmentbuddy.R
+
+class AppointmentHome : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_appointment_home, container, false)
+        // Accessing Save button from Fragment 2
+        val back: Button = view.findViewById(R.id.appointment_back)
+        val new_appointment: CardView = view.findViewById(R.id.newAppointment)
+        new_appointment.setOnClickListener {
+//                view.findNavController().navigate()
+        }
+        return inflater.inflate(R.layout.fragment_appointment_home, container, false)
+    }
+}
+
