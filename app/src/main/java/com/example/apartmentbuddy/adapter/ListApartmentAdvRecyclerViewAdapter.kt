93d077c72969a6aa550ea488c85016e3a4e5f68e
Minal Khona <mn977442@dal.ca>
2022-11-19 21:54:35 -0400

    Bookmark posts

--- app/src/main/java/com/example/apartmentbuddy/adapter/ListApartmentAdvRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/apartmentbuddy/adapter/ListApartmentAdvRecyclerViewAdapter.kt
@@ -8,8 +8,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.apartmentbuddy.R
+import com.example.apartmentbuddy.controller.AdvertisementController
 import com.example.apartmentbuddy.model.Apartment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import me.relex.circleindicator.CircleIndicator
 
 /**
@@ -24,6 +26,8 @@ class ListApartmentAdvRecyclerViewAdapter(
     lateinit var viewPagerAdapter: ImageSliderViewPagerAdapter
     lateinit var context: Context
     lateinit var indicator: CircleIndicator
+    lateinit var bookmark: FloatingActionButton
+    lateinit var bookmarkRemove: FloatingActionButton
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -31,24 +35,30 @@ class ListApartmentAdvRecyclerViewAdapter(
     ): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.recycler_apartments, parent, false)
-        if (bottomNavValue == "myPosts") {
-            view.findViewById<FloatingActionButton>(R.id.edit)?.visibility = View.VISIBLE
-            view.findViewById<FloatingActionButton>(R.id.delete)?.visibility = View.VISIBLE
-            view.findViewById<FloatingActionButton>(R.id.bookmark)?.visibility = View.INVISIBLE
+
+        bookmark = view.findViewById(R.id.bookmark)
+        bookmarkRemove = view.findViewById(R.id.bookmark_remove)
+
+        when (bottomNavValue) {
+            "myPosts" -> {
+                view.findViewById<FloatingActionButton>(R.id.edit)?.visibility = View.VISIBLE
+                view.findViewById<FloatingActionButton>(R.id.delete)?.visibility = View.VISIBLE
+                view.findViewById<FloatingActionButton>(R.id.bookmark)?.visibility = View.INVISIBLE
+            }
+            "bookmark" -> {
+                view.findViewById<FloatingActionButton>(R.id.bookmark_remove)?.visibility =
+                    View.VISIBLE
+            }
         }
-        if (bottomNavValue == "bookmark") {
-            view.findViewById<FloatingActionButton>(R.id.bookmark_remove)?.visibility = View.VISIBLE
-        }
+
         viewPager = view.findViewById(R.id.idViewPager)
         indicator = view.findViewById(R.id.indicator)
         context = parent.context
+
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(
-        holder: ViewHolder,
-        position: Int
-    ) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val advertisementItem = listings[position]
 
         viewPagerAdapter = ImageSliderViewPagerAdapter(context, advertisementItem.images)
@@ -62,6 +72,36 @@ class ListApartmentAdvRecyclerViewAdapter(
         holder.rent.text = advertisementItem.rent.toString()
         holder.startDate.text = advertisementItem.startDate
         holder.contact.text = advertisementItem.contact
+
+        //displayRemoveBookmarkFab(advertisementItem.bookmarkUserList, holder)
+
+        //TODO(): Compare with loggedIn user's ID
+        val loggedInUser = advertisementItem.uid
+        if (advertisementItem.bookmarkUserList?.map { string ->
+                string.replace("[", "").replace("]", "")
+            }?.contains(loggedInUser) == true) {
+            holder.bookmarkRemove.visibility = View.VISIBLE
+        }
+
+        //Bookmark the post
+        bookmark.setOnClickListener {
+            Snackbar.make(it, "Post Saved For Later", 2000).show()
+            AdvertisementController().addUserToBookmarkList(
+                advertisementItem.documentId,
+                //TODO(): Pass loggedIn USER ID
+                advertisementItem.uid
+            )
+        }
+
+        //Remove Bookmark
+        bookmarkRemove.setOnClickListener {
+            Snackbar.make(it, "Bookmark Removed", 2000).show()
+            AdvertisementController().removeUserToBookmarkList(
+                advertisementItem.documentId,
+                //TODO(): Pass loggedIn USER ID
+                advertisementItem.uid
+            )
+        }
     }
 
     override fun getItemCount(): Int {
@@ -76,5 +116,18 @@ class ListApartmentAdvRecyclerViewAdapter(
         val rent: TextView = itemView.findViewById(R.id.apartmentRent)
         val startDate: TextView = itemView.findViewById(R.id.apartmentAvailability)
         val contact: TextView = itemView.findViewById(R.id.apartmentContact)
+        val bookmark: FloatingActionButton = itemView.findViewById(R.id.bookmark)
+        val bookmarkRemove: FloatingActionButton = itemView.findViewById(R.id.bookmark_remove)
+    }
+
+    fun displayRemoveBookmarkFab(list: MutableList<String>?, holder: ViewHolder) {
+        //TODO(): Compare with loggedIn user's ID
+        val loggedInUser = "Minal"
+
+        if (list?.map { string ->
+                string.replace("[", "").replace("]", "")
+            }?.contains(loggedInUser) == true) {
+            holder.bookmarkRemove.visibility = View.VISIBLE
+        }
     }
 }
