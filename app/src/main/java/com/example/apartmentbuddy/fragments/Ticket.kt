288fb78c55a1f84b53e69f9fbaf17ca2ff643766
Saifali <sf527330@dal.ca>
2022-11-20 14:22:21 -0400

    Added firestore dependency and wrote code to fetch data from firestore database.

--- app/src/main/java/com/example/apartmentbuddy/fragments/Ticket.kt
+++ app/src/main/java/com/example/apartmentbuddy/fragments/Ticket.kt
@@ -1,6 +1,7 @@
 package com.example.apartmentbuddy.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +11,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.apartmentbuddy.R
 import com.example.apartmentbuddy.adapter.TicketAdapter
+import com.example.apartmentbuddy.model.AppointmentData
 import com.example.apartmentbuddy.model.Tickets
+import com.google.firebase.firestore.*
 
 class Ticket : Fragment() {
     lateinit var recyclerView: RecyclerView
     lateinit var list: MutableList<Tickets>
     lateinit var adapter: TicketAdapter
+    private val db = FirebaseFirestore.getInstance()
+    private val appointmentCollection = db.collection("appointment")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,9 +36,34 @@ class Ticket : Fragment() {
         val language = resources.getStringArray(R.array.status_options)
         val statusAdapter = ArrayAdapter(requireContext(), R.layout.list_item,language)
         list = mutableListOf()
-        loadData()
         adapter = TicketAdapter(requireContext(),list, statusAdapter)
         recyclerView.adapter = adapter
+        EventChangeListener()
+
+
+    }
+
+    private fun EventChangeListener() {
+        appointmentCollection.addSnapshotListener(object: EventListener<QuerySnapshot> {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if(error != null) {
+                    Log.e("Firestore Error",error.message.toString())
+                    return
+                }
+                for(dc: DocumentChange in value?.documentChanges!!) {
+                    if(dc.type == DocumentChange.Type.ADDED) {
+                        val name: String? = dc.document.getString("name")
+                        val status: String? = dc.document.getString("date")
+                        val date: String? = dc.document.getString("time")
+                        list.add(Tickets(name,status,date));
+                        println(list)
+
+                    }
+
+                }
+            }
+        })
+
 
     }
 
