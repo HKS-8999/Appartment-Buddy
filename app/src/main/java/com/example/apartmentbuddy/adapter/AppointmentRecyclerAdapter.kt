09a810e9819d4e33401bb64abeae0784af3aaee8
Harsh Shah <hr838264@dal.ca>
2022-11-22 21:22:15 -0400

    Added cancel appointment Fragment

--- app/src/main/java/com/example/apartmentbuddy/adapter/AppointmentRecyclerAdapter.kt
+++ app/src/main/java/com/example/apartmentbuddy/adapter/AppointmentRecyclerAdapter.kt
@@ -3,18 +3,27 @@ package com.example.apartmentbuddy.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.apartmentbuddy.R
+import com.example.apartmentbuddy.fragments.CancelAppointmentDirections
+import com.example.apartmentbuddy.fragments.ShowAppointmentDirections
 import com.example.apartmentbuddy.model.AppointmentData
+import com.example.apartmentbuddy.model.ShowAppointmentData
+import com.google.firebase.firestore.QueryDocumentSnapshot
 
-class AppointmentRecyclerAdapter (private val appointment: List<AppointmentData>) : RecyclerView.Adapter<AppointmentRecyclerAdapter.ViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class AppointmentRecyclerAdapter(private val appointment: List<ShowAppointmentData>) : RecyclerView.Adapter<AppointmentRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_view_each_appointment,parent,false)
             return ViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.appointmentId.text = appointment[position].appointment_id
             holder.appointmentDate.text = appointment[position].date
             holder.appointmentTime.text = appointment[position].time
             holder.appointmentLocation.text = appointment[position].location
@@ -28,12 +37,18 @@ class AppointmentRecyclerAdapter (private val appointment: List<AppointmentData>
             var appointmentDate : TextView
             var appointmentTime : TextView
             var appointmentLocation : TextView
+            lateinit var appointmentId : TextView
+//            var thisAppointment : LinearLayout
 
             init {
+                appointmentId = itemView.findViewById(R.id.show_appointment_count)
                 appointmentDate = itemView.findViewById(R.id.show_appointment_date)
                 appointmentTime = itemView.findViewById(R.id.show_appointment_time)
                 appointmentLocation = itemView.findViewById(R.id.show_appointment_location)
+//                thisAppointment = itemView.findViewById(R.id.each_appointment)
+//                thisAppointment.setOnClickListener {
+////                    itemView.findNavController().navigate(CancelAppointmentDirections.)
+//                }
             }
         }
     }
-}
--- app/src/main/java/com/example/apartmentbuddy/model/AppointmentData.kt
+++ app/src/main/java/com/example/apartmentbuddy/model/AppointmentData.kt
@@ -6,5 +6,14 @@ data class AppointmentData(
     val time:String,
     val user_id: String,
     var location: String,
-    val timestamp: String
-)
+    val timestamp: String,
+){
+
+    constructor(name:String,
+                date: String,
+                time:String,
+                user_id: String,
+                location: String,
+                timestamp: String,
+                appointment_id : String) : this(name, date, time, user_id, location, timestamp)
+}
